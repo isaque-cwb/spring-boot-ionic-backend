edd410f1d3c055732380daf7e41ad1c72e16f339
Isaque Lourenco <isaque.lourenco2@gmail.com>
2020-04-17 03:08:33 -0300

    Ajustes no endpoint /categorias/{id}

---
+++ src/main/java/com/isaquelourenco/cursomc/resources/exception/ResouceExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.isaquelourenco.cursomc.resources.exception;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.isaquelourenco.cursomc.services.exceptions.ObjectNotFoundException;
+
+@ControllerAdvice
+public class ResouceExceptionHandler {
+	
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request){
+		StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+	}
+}
