8444208455f385c269b8eafd5bf67727c5b9a26a
Isaque Lourenco <isaque.lourenco2@gmail.com>
2020-04-19 00:34:53 -0300

    Inserindo Pedido

--- src/main/java/com/isaquelourenco/cursomc/domain/PagamentoComCartao.java
+++ src/main/java/com/isaquelourenco/cursomc/domain/PagamentoComCartao.java
@@ -2,9 +2,11 @@ package com.isaquelourenco.cursomc.domain;
 
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonTypeName;
 import com.isaquelourenco.cursomc.enums.EstadoPagamento;
 
 @Entity
+@JsonTypeName("pagamentoComCartao")
 public class PagamentoComCartao extends Pagamento {
 	private static final long serialVersionUID = 1L;
 	
